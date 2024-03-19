f814d9fb642870df78b5fd30ff5310af2bd206de
Taneli <Taneli@TANELIN-PC>
2024-03-19 22:50:38 +0200

    Add project files.

---
+++ Taneli_Terava_PullRequestAssignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Taneli_Terava_PullRequestAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
